99c9ab655b56339d1922cc2d1311c50df7c7f0be
adhyuva <adhyuva8@gmail.com>
2021-03-20 12:50:43 -0400

    Fading in and adding a second question

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+document.getElementById("submit").addEventListener("click", ifLoop);
 function ifLoop () {
     let question1 = document.getElementById("bytes").value;
     let one = document.getElementById("one");
@@ -5,13 +6,15 @@ function ifLoop () {
     let two = document.getElementById("two");
     let question3 = document.getElementById("bytes-2").value;
     let three = document.getElementById("three");
+    let question4 = document.getElementById("data").value;
+    let four = document.getElementById("four");
     let score = 0;
     let output = document.getElementById("score");
     if(question1 == "1024"){
         one.innerHTML="<font color='green'>CORRECT!</font>";
         score += 1; 
     }else if (question1 == ""){
-        one.innerHTML="<font color='gray'>Nothing Written.</font>"
+        one.innerHTML="<font color='gray'>Nothing Written.</font>";
     }else{
         one.innerHTML="<font color='red'>INCORRECT!</font>";
       }
@@ -19,7 +22,7 @@ function ifLoop () {
         two.innerHTML="<font color='green'>CORRECT!</font>";
         score += 1; 
     }else if (question2 == ""){
-        two.innerHTML="<font color='gray'>Nothing Written.</font>"
+        two.innerHTML="<font color='gray'>Nothing Written.</font>";
     }else{
         two.innerHTML="<font color='red'>INCORRECT!</font>";
     }
@@ -31,15 +34,26 @@ function ifLoop () {
     }else{
         three.innerHTML="<font color='red'>INCORRECT!</font>";
     }
-    output.innerHTML = "Score:" + score + "/4";
-    if (score == 4){
-        output.innerHTML += " <br/> &#128170; Awesome! You know your facts! &#128170; ";
+    if(question4 == "data"){
+        four.innerHTML="<font color='green'>CORRECT!</font>";
+        score += 2; 
+    }else if (question4 == ""){
+        four.innerHTML="<font color='gray'>Nothing Written.</font>";
+    }else{
+        four.innerHTML="<font color='red'>INCORRECT!</font>";
+    }
+    output.innerHTML = "Score:" + (score/4)*100  + "%";
+    if (score > 4){
+        output.innerHTML += " <br/> &#128170; Your an A++ student! &#128170; ";
+      }else if (score == 4){
+        output.innerHTML += " <br/> &#128077; Nice! You got an A+! &#128077;";
       }else if (score >= 2){
         output.innerHTML += " <br/> &#128077; Your familiar with binary.&#128077;";
       }else if (question1==""&&question2==""&&question3==""){
         output.innerHTML = " <br/> &#128533;We can not give you a score because everything is blank.&#128533;<br/> Please try again!";
       }else{
-        output.innerHTML += " <br/>&#128218; You do not know binary. Study more! &#128218; ";
+        output.innerHTML += " <br/>&#128218; You do not know binary. Study more!.  &#128218; ";
       }
 
 }
+$("body").hide().fadeIn(500);
